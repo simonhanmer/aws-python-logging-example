e4b1639cda13eece00ba9d93574b3a9d92801c45
Simon Hanmer <Simon.Hanmer@ecs.co.uk>
2022-09-21 14:34:38 +0100

    git add lambda file

---
+++ source/lambda/eventDumper/lambda_function.py
@@ -0,0 +1,43 @@
+import os
+import json
+import logging
+import botocore
+import boto3
+
+
+logLevel = os.getenv("LOG_LEVEL", "DEBUG").upper()
+logging.basicConfig(level=logLevel)
+logger   = logging.getLogger()
+
+
+def lambda_handler(event, context):
+    demo_logger()
+    list_buckets()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+
+def demo_logger():
+    logger.debug(f"D: In {__name__}")
+    logger.info(f"I: In {__name__}")
+    logger.warning(f"W: In {__name__}")
+    logger.error(f"E: In {__name__}")
+    logger.critical(f"C: In {__name__}")
+
+
+def list_buckets():
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.list_buckets()
+    except botocore.exceptions.ClientError as exception:
+        logger.exception(f"Unexpected Error: {exception}")
+    for bucket in response['Buckets']:
+        logger.info(f"Processing bucket {bucket['Name']}")
+
+
+
+if __name__ == '__main__':
+    demo_logger()
+    list_buckets()
